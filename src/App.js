efd15203a314bfae2cb2b767ed311638c4ebac60
eliabearaujo <eliabe_trajano@hotmail.com>
2023-05-21 23:54:11 -0300

    Qualifications started.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Skills from './Components/Skills/Skills';
 import Services from './Components/Services/Services';
+import Qualification from './Components/Qualification/Qualification';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <About />
         <Skills />
         <Services />
+        <Qualification />
       </main>
     </>
   );
---
+++ src/Components/Qualification/Qualification.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Qualification.css';
+
+const Qualification = () => {
+  return <div>Qualification</div>;
+};
+
+export default Qualification;
