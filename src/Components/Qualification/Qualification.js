26860652b5b3e596fd6556d6bd3ee7ef44d68bad
eliabearaujo <eliabe_trajano@hotmail.com>
2023-05-22 00:00:12 -0300

    Qualification tabs created.

--- src/Components/Qualification/Qualification.js
+++ src/Components/Qualification/Qualification.js
@@ -2,7 +2,26 @@ import React from 'react';
 import './Qualification.css';
 
 const Qualification = () => {
-  return <div>Qualification</div>;
+  return (
+    <section className="qualification section" id="qualification">
+      <h2 className="section_title">Qualification</h2>
+      <span className="section_subtitle">My personal journey</span>
+
+      <div className="qualification_container container">
+        <div className="qualification_tabs">
+          <div className="qualification_button qualification_active button--flex">
+            <i className="uil uil-graduation-cap qualification_icon"></i>
+            Education
+          </div>
+
+          <div className="qualification_button button--flex">
+            <i className="uil uil-briefcase-alt qualification_icon"></i>
+            Experience
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Qualification;
