01a62297d43d09529da6ec22eb54139325dc8174
eliabearaujo <eliabe_trajano@hotmail.com>
2023-06-01 02:29:09 -0300

    Works iniciado.

--- src/Components/Work/Works.js
+++ src/Components/Work/Works.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { projectsData } from './Data';
 import { projectsNav } from './Data';
+import WorksItems from './WorksItems';
 
 const Works = () => {
   return (
-    <>
+    <div>
       <div className="work_filters">
         {projectsNav.map((item, index) => {
           return (
@@ -14,7 +15,12 @@ const Works = () => {
           );
         })}
       </div>
-    </>
+      <div className="work_container container grid">
+        {projectsData.map((item) => {
+          return <WorksItems />;
+        })}
+      </div>
+    </div>
   );
 };
 
---
+++ src/Components/Work/WorksItems.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WorksItems = () => {
+  return <div>WorksItems</div>;
+};
+
+export default WorksItems;
