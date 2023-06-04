1dd7fe7bb78137844334a2be1af0e2da1b344150
eliabearaujo <eliabe_trajano@hotmail.com>
2023-06-04 05:08:49 -0300

    Work finished.

--- src/Components/Work/WorkItems.js
+++ src/Components/Work/WorkItems.js
@@ -5,7 +5,7 @@ const WorksItems = ({ item }) => {
     <div className="work_card" key={item.id}>
       <img src={item.image} alt="" className="work_image" />
       <h3 className="work_title">{item.title}</h3>
-      <a href="" className="work_button">
+      <a href="#" className="work_button">
         Demo <i className="bx bx-right-arrow-alt work_button_icon"></i>
       </a>
     </div>
