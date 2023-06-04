1829904b2a96dfb718ba5f83c7c707dce82ebc74
eliabearaujo <eliabe_trajano@hotmail.com>
2023-06-04 04:53:56 -0300

    Work criado.

--- src/Components/Work/Works.js
+++ src/Components/Work/Works.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { projectsData } from './Data';
 import { projectsNav } from './Data';
-import WorksItems from './WorksItems';
+import WorkItems from './WorkItems';
 
 const Works = () => {
+  const [item, setItem] = React.useState({ name: 'all' });
+  const [projects, setProjects] = useState([]);
+  const [active, setActive] = useState(0);
+
+  useEffect(() => {
+    if (item.name === 'all') {
+      setProjects(projectsData);
+    } else {
+      const newProjects = projectsData.filter((project) => {
+        return project.category === item.name;
+      });
+      setProjects(newProjects);
+    }
+  }, [item]);
+
+  const handleClick = (e, index) => {
+    setItem({ name: e.target.textContent });
+    setActive(index);
+  };
   return (
     <div>
       <div className="work_filters">
         {projectsNav.map((item, index) => {
           return (
-            <span className="work_item" key={index}>
+            <span
+              onClick={(e) => {
+                handleClick(e, index);
+              }}
+              className={`${active === index ? 'active_work' : ''} work_item`}
+              key={index}
+            >
               {item.name}
             </span>
           );
         })}
       </div>
       <div className="work_container container grid">
-        {projectsData.map((item) => {
-          return <WorksItems />;
+        {projects.map((item) => {
+          return <WorkItems item={item} key={item.id} />;
         })}
       </div>
     </div>
--- src/Components/Work/WorksItems.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const WorksItems = () => {
-  return <div>WorksItems</div>;
-};
-
-export default WorksItems;
